e91e030013583e51227112c2a787c0aa1614dcd1
Victoria-Gudoit <vgudojt@mail.ru>
2022-12-22 20:54:15 +0300

    add products block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,14 @@ import { useState } from "react";
 import { Header } from "./Header";
 import scss from "./app.module.scss";
 import { Hero } from "./Hero";
+import { Products } from "./products";
 
 export const App = () => {
   return (
     <div className={scss.wrapper}>
       <Header />
       <Hero />
+      <Products />
     </div>
   );
 };
